f0bcdaee485b4d6b9f59b86360656dc812023ff0
jaki <jaki@jaki.me>
2023-12-25 10:17:26 +0800

    fix sql

--- common/dal/quest.go
+++ common/dal/quest.go
@@ -31,5 +31,5 @@ const (
 	UpdateCampaignInfoSql             = `update quest_campaign_info set total_reward=$1,total_users=$2,total_quest_execution=$3,updated_at=$4`
 	UpdateQuestCampaignTotalUsersSql  = `update quest_campaign set total_users=$1,updated_at=$2 where id=$3`
 
-	FindQuestLongSql = `select id,rule from quest_long where category=$1 and statys=$2`
+	FindQuestLongSql = `select id,rule from quest_long where category=$1 and status=$2`
 )
